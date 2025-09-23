cfedae703affdac204a8aaeb832b3c34f98ddae7
Jonas <jonashahn1@gmx.net>
2025-09-23 22:59:18 +0200

    Update print.py

--- print.py
+++ print.py
@@ -4,8 +4,6 @@ import time
 # Setup
 GPIO.setmode(GPIO.BCM)
 PIN = 26
-
-# Setup pin as input with pull-up
 GPIO.setup(PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 print(f"Monitoring GPIO{PIN}. Press Ctrl+C to exit.")
